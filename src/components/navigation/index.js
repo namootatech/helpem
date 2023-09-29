4f2305556c0389eec0800fe5b28fd7dc807ac0a4
Ayabonga Qwabi <aya@simply.co.za>
2023-09-29 13:39:33 +0200

    fix: transparent navigation

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -6,7 +6,7 @@ const Navigation = () => {
   const toggleIsOpen = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="bg-transparent dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="https://flowbite.com/" className="flex items-center">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
