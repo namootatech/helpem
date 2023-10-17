694e672b6b382ad5e36121f016c7c281c430c662
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2023-10-17 22:38:28 +0200

    feat: add login button

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -8,7 +8,7 @@ const Navigation = () => {
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="https://flowbite.com/" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Help&apos;em
           </span>
@@ -20,6 +20,13 @@ const Navigation = () => {
             className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Get started
+          </Link>
+          <Link
+            type="button"
+            href="/login"
+            className="text-white bg-black hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ml-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+           Login
           </Link>
           <button
             data-collapse-toggle="navbar-sticky"
