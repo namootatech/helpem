2c355d0d125858d5c16990ffdc060cb18d6c6a91
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2023-10-17 22:15:04 +0200

    refactor: restyle about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,14 +13,14 @@ export default function Home() {
     <Layout>
       <div className="container mt-4 px-16 py-20">
         <h1 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">Feeding Hope in Africa</h1>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl dark:text-gray-400 mt-2 mb-12">
           Welcome to Help'em, a dedicated initiative by Food on Every Table NPO
           with a singular mission - to end hunger and bring hope to communities
           in need across Africa.
         </p>
 
         <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">Our Story</h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl dark:text-gray-400 mt-2 mb-12">
           Help'em was born from a deep sense of responsibility and a shared
           commitment to address one of the most pressing challenges facing
           Africa - hunger. Our journey began in 2018,
@@ -32,14 +32,14 @@ export default function Home() {
         </p>
 
         <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">Our Mission</h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl dark:text-gray-400 mt-2 mb-12">
           At Help'em, we believe that no one should endure the pain of an empty
           stomach. Our mission is clear - to provide nourishment to those in
           need and to restore hope in the hearts of the underserved.
         </p>
 
         <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">What We Do</h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl dark:text-gray-400 mt-2 mb-12">
           Help'em is more than an initiative; it's a lifeline. We work
           tirelessly to produce nutritious food bars, but our mission extends
           beyond that. We make it our duty to ensure that these life-saving
@@ -48,7 +48,7 @@ export default function Home() {
         </p>
 
         <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white my-2">Our Values</h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl dark:text-gray-400 mt-2 mb-12">
           At Help'em, our values serve as our guiding light. They include "compassion," "transparency,"
           "collaboration," and these principles underpin everything we do. We
           take pride in our commitment to complete transparency in our
@@ -57,7 +57,7 @@ export default function Home() {
         </p>
 
         <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">Meet Our Team</h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl dark:text-gray-400 mt-2 mb-12">
           Our success is a testament to the dedication and passion of our team.
           Meet the individuals who drive Help'em forward, from our founders to
           our committed volunteers. Together, we share a common goal - to
@@ -65,7 +65,7 @@ export default function Home() {
           most.
         </p>
         <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">Join the Movement</h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl dark:text-gray-400 mt-2 mb-12">
           {" "}
           Help'em is more than an initiative; it's a movement of compassion,
           driven by the belief that change is possible when people come
--- src/pages/cause.js
+++ src/pages/cause.js
@@ -12,19 +12,19 @@ export default function Home() {
   return (
     <Layout>
       <div className="container mt-4 px-16 py-20">
-        <h1 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">
+        <h1 class="text-4xl font-semibold leading-normal  dark:text-white mb-2">
           Ending Hunger in Africa
         </h1>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl mt-2 mb-12">
           At Help'em, our cause is simple yet profound - to end hunger and bring
           sustenance to the people of Africa. Discover why our mission matters,
           the challenges we face, and how you can be a part of the solution.
         </p>
 
-        <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">
+        <h4 class="text-4xl font-semibold leading-normal  dark:text-white mb-2">
           The Challenge
         </h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl mt-2 mb-12">
           Hunger in Africa is a pressing and complex issue. Despite the
           continent's abundant resources, millions of people go to bed hungry
           every night. The causes are numerous, including poverty, food
@@ -33,10 +33,10 @@ export default function Home() {
           well-being of the entire continent.
         </p>
 
-        <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">
+        <h4 class="text-4xl font-semibold leading-normal  dark:text-white mb-2">
           Why Our Cause Matters
         </h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl mt-2 mb-12">
           <ol>
             <li>
               <strong>Human Dignity:</strong> Hunger robs individuals of their
@@ -44,17 +44,20 @@ export default function Home() {
               circumstances, deserves the basic human right of access to
               nutritious food.
             </li>
+            <br/>
             <li>
               <strong>Community Resilience: </strong>When communities have
               access to consistent nourishment, they become more resilient and
               better equipped to overcome other challenges they face.
             </li>
+            <br/>
             <li>
               <strong>Health and Education:</strong> Adequate nutrition is the
               foundation for good health and educational attainment. By
               eradicating hunger, we open doors to a brighter future for
               generations to come.
             </li>
+            <br/>
             <li>
               <strong>Sustainable Development:</strong> Addressing hunger is a
               crucial step towards achieving the United Nations Sustainable
@@ -63,23 +66,28 @@ export default function Home() {
           </ol>
         </p>
 
-        <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">
+        <h4 class="text-4xl font-semibold leading-normal  dark:text-white mb-2">
           Our Approach
         </h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
-          Help'em takes a holistic approach to tackling hunger in Africa. We are
-          committed to:
+        <p class="text-left text-2xl mt-2 mb-12">
+          Help'em takes a holistic approach to tackling hunger in Africa.
+          <br/>
+          We are committed to:
+          <br/>
+          <br/>
           <ul>
             <li>
               <strong>Producing Nutritious Food Bars:</strong> We produce
               high-quality food bars that serve as a convenient and nourishing
               source of sustenance for those in need.
             </li>
+            <br/>
             <li>
               <strong>Distribution:</strong> Our dedicated team and partners
               ensure that these food bars reach the most vulnerable and remote
               communities across the continent.
             </li>
+            <br/>
             <li>
               <strong>Transparency: </strong>We are committed to complete
               transparency in our operations and how your contributions are
@@ -88,32 +96,32 @@ export default function Home() {
           </ul>
         </p>
 
-        <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white my-2">
+        <h4 class="text-4xl font-semibold leading-normal  dark:text-white my-2">
           Join Us in the Fight Against Hunger
         </h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl mt-2 mb-12">
           Our cause is a call to action. Hunger in Africa is a complex issue,
           but it is one that can be solved with your support. By subscribing,
           volunteering, or partnering with Help'em, you become part of a
           movement to end hunger and bring hope to the people of Africa.
         </p>
 
-        <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">
+        <h4 class="text-4xl font-semibold leading-normal  dark:text-white mb-2">
           Meet Our Team
         </h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl mt-2 mb-12">
           Our success is a testament to the dedication and passion of our team.
           Meet the individuals who drive Help'em forward, from our founders to
           our committed volunteers. Together, we share a common goal - to
           eradicate hunger in Africa and to bring hope to those who need it
           most.
         </p>
-        <h4 class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white mb-2">
-          JMake a Difference Today
+        <h4 class="text-4xl font-semibold leading-normal  dark:text-white mb-2">
+          Make a Difference Today
         </h4>
-        <p class="text-left text-gray-500 dark:text-gray-400 mt-2 mb-12">
+        <p class="text-left text-2xl mt-2 mb-12">
           {" "}
-          HYour support can change lives, one meal at a time. Join us in the
+          Your support can change lives, one meal at a time. Join us in the
           fight against hunger in Africa and be a part of a brighter future for
           all.
           
