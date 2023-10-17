a1caaff15d51e5e428871a0ba59fe4b412f9582f
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2023-10-17 22:38:00 +0200

    feat: add progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import NextNProgress from 'nextjs-progressbar';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NextNProgress color="#b91c1c" />
+      <Component {...pageProps} />;
+    </>
+  );
 }
