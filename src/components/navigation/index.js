e03dbfe80f702ddca1a04c1cc9a05bbf2ce17186
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2023-10-18 00:35:29 +0200

    feat: implement blogging

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -94,6 +94,14 @@ const Navigation = () => {
                 Contact
               </Link>
             </li>
+            <li>
+              <Link
+                href="/blog"
+                className=" text-xl block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:dark:hover:text-red-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Blog
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
