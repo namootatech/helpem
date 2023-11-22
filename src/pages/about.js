9e79bf07fd509c35fa9fe6083972afaa0a96517c
Ayabonga Qwabi <aya@simply.co.za>
2023-11-23 00:22:34 +0200

    chore: clean

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,5 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Layout from "@/components/layout";
-import Mission from "@/components/mission";
-import WhyHelpEm from "@/components/whyhelpem";
-import GetInvolved from "@/components/getInvolved";
-import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
