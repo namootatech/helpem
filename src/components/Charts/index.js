0a6da17e5dc0fa75027422514abb500de81f7150
Ayabonga Qwabi <aya@simply.co.za>
2024-03-13 19:17:22 +0200

    fix builde issue

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,8 +1,8 @@
 "use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import ChartOne from "@/components/Charts/ChartOne";
-import ChartTwo from "@/components/Charts/ChartTwo";
-import ChartThree from "@/components/Charts/ChartThree";
+// import ChartOne from "@/components/Charts/ChartOne";
+// import ChartTwo from "@/components/Charts/ChartTwo";
+// import ChartThree from "@/components/Charts/ChartThree";
 import React from "react";
 
 const Chart=() => {
@@ -11,9 +11,9 @@ const Chart=() => {
       <Breadcrumb pageName="Chart" />
 
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
-        <ChartOne />
+        {/* <ChartOne />
         <ChartTwo />
-        <ChartThree />
+        <ChartThree /> */}
       </div>
     </>
   );
