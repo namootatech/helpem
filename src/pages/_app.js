7a4407ccfbf70a67068f7657a7bfb1a7b893e0e4
Ayabonga Qwabi <aya@simply.co.za>
2024-03-14 20:31:04 +0200

    add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,30 @@
-import "@/styles/globals.css";
-import NextNProgress from "nextjs-progressbar";
-import { Provider } from "react-redux";
-import store from "@/store";
-import { getThemeConfig } from "@/themes";
+import '@/styles/globals.css';
+import NextNProgress from 'nextjs-progressbar';
+import { Provider } from 'react-redux';
+import store from '@/store';
+import { getThemeConfig } from '@/themes';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }) {
-  const theme = getThemeConfig()
+  const theme = getThemeConfig();
   return (
     <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
+          page_path: window.location.pathname,
+          
+![image](https://cdn.sanity.io/images/dgsq0x0m/production/61f24596fb210134e5df6a1dfa285de0f24791cc-1549x601.png?w=450)});
+         `}
+      </Script>
       <Provider store={store}>
         <NextNProgress color={theme.progressColor} />
         <Component {...pageProps} />;
